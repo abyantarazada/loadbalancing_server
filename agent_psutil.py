d95d8568cd8294e4f9a0aac05fb6c4e8f261fbf1
abyantarazada <49398391+abyantarazada@users.noreply.github.com>
2022-06-13 21:03:34 +0700

    Update agent_psutil.py

--- agent_psutil.py
+++ agent_psutil.py
@@ -4,18 +4,10 @@ import psutil
 import pickle
 import time
 
-def memory_usage():
-    memory = psutil.virtual_memory().percent
-    return pickle.dumps(memory)
-
-def cpu_usage():
-    cpu = psutil.cpu_percent()
-    return pickle.dumps(cpu)
-
 def server_usage():
-    usage = (memory_usage() + cpu_usage())/2
+    usage = (psutil.virtual_memory().percent + psutil.cpu_percent())/2
     return pickle.dumps(usage)
-
+    
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 while True:
